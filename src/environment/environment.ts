d90b551733f4f62f48b3026bff23af884e1e0dd6
Sanuka <sanuka.20232509@iit.ac.lk>
2025-11-14 17:20:55 +0530

    added environment variables

---
+++ src/environment/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  keycloak: {
+    url: 'http://localhost:8080',
+    realm: 'shophub',
+    clientId: 'backend-client',
+  },
+  apiServiceUrl: 'http://localhost:9002',
+};
+
