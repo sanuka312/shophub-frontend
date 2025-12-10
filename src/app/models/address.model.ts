c3ee1898f0d5913bca7d0e0a6d0e8da22b82051f
Sanuka <sanuka.20232509@iit.ac.lk>
2025-12-10 10:49:15 +0530

    added the address model

---
+++ src/app/models/address.model.ts
@@ -0,0 +1,14 @@
+export interface Address {
+  address_id?: number;
+  keycloak_user_id?: string;
+  first_name?: string;
+  last_name?: string;
+  email?: string;
+  phone?: string;
+  line1?: string;
+  line2?: string;
+  city: string;
+  postal_code?: string;
+  country?: string;
+}
+
